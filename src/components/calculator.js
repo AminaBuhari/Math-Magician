8c7f77a600d6f46c605abbf320cc106619921928
buhariaminat <buhariaminat@yahoo.com>
2022-04-21 22:21:34 +0100

    import calculator from logic

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import calculate from './logic/calculator';
+import calculate from './logic/Calculator';
 
 class Calculator extends React.Component {
   constructor(props) {
