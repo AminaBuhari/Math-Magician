5c3b03a43419fc4c27389dade94a0cddf1540208
buhariaminat <buhariaminat@yahoo.com>
2022-04-21 22:24:30 +0100

    Fix linters

--- src/components/events.js
+++ src/components/events.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import calculate from './logic/Calculator';
+import calculate from './logic/calculator';
 
 class Calculator extends React.Component {
   constructor(props) {
