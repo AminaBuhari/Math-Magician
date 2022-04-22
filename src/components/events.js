d30c33fe16af934a2674d6a4b4a3deb64ddd8fcb
buhariaminat <buhariaminat@yahoo.com>
2022-04-22 14:32:24 +0100

    setstate of object

--- src/components/events.js
+++ src/components/events.js
@@ -2,9 +2,29 @@ import React from 'react';
 import calculate from './logic/calculator';
 
 const Calculator = () => {
-  const datas = [{ label: 'AC'}, { label: '+/-'}, { label: '/'}, { label: '%'}, { label: '7'}, { label: '8'}, { label: '9'}, { label: 'x' }, { label: '4'}, { label: '5' }, { label: '6'}, { label: '-'}, { label: '1'}, { label: '2'}, { label: '3'}, { label: '+' }, { label: '0' }, { label: '.' }, { label: '=' }];
+  const datas = [
+    { label: 'AC' },
+    { label: '+/-' },
+    { label: '/' },
+    { label: '%' },
+    { label: 7 },
+    { label: 8 },
+    { label: 9 },
+    { label: 'x' },
+    { label: 4 },
+    { label: 5 },
+    { label: 6 },
+    { label: '-' },
+    { label: 1 },
+    { label: 2 },
+    { label: 3 },
+    { label: '+' },
+    { label: 0 },
+    { label: '.' },
+    { label: '=' },
+  ];
 
-  const [operate, setState] = React.useState({
+  const [operates, setState] = React.useState({
     total: null,
     next: null,
     operation: null,
@@ -12,23 +32,26 @@ const Calculator = () => {
 
   const handleChanger = (event) => {
     const value = event.target.textContent;
-    const object = calculate(operate, value);
-    const { total, next} = object;
-    setState({ operate: object });
+    const { total, next, operation } = calculate(operates, value);
+    setState({ total, next, operation });
     const answer = document.querySelector('.console');
     if (next !== null) (answer.innerHTML = next);
     else if (total !== null) (answer.innerHTML = total);
-    else (answer.innerHTML = 0);
+    else answer.innerHTML = 0;
   };
 
   return (
     <div className="wrapper">
       <div className="console">0</div>
       {datas.map((data) => (
-        <button type="button" key={data.label} onClick={handleChanger} className="container">
+        <button
+          type="button"
+          key={data.label}
+          onClick={handleChanger}
+          className="container"
+        >
           {data.label}
         </button>
-
       ))}
     </div>
   );
