be051327af7b0d92904f3d9cb510512f334d2489
buhariaminat <buhariaminat@yahoo.com>
2022-04-28 04:05:37 +0100

    testing routers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/events';
+import About from './components/about';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
       <header className="header">
         <Calculator />
       </header>
